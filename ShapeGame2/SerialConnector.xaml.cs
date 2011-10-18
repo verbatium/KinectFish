9fa52303d58bd91c48e70be746ca33eff2807096
Rasmus Raag <rasmus.raag@gmail.com>
2011-10-18 13:36:51 +0300

    Added a new window to choose serial ports at startup

---
+++ ShapeGame2/SerialConnector.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShapeGame2
+{
+    /// <summary>
+    /// Interaction logic for SerialConnector.xaml
+    /// </summary>
+    public partial class SerialConnector : Window
+    {
+        public SerialConnector()
+        {
+            InitializeComponent();
+        }
+    }
+}
