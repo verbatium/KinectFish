58e937d9c60b2fd31257751686d6395276bc3a95
Rasmus Raag <rasmus.raag@gmail.com>
2011-10-18 16:35:21 +0300

    Serialport settings are stored when configuration window is closed.

--- ShapeGame2/SerialConnector.xaml.cs
+++ ShapeGame2/SerialConnector.xaml.cs
@@ -19,9 +19,15 @@ namespace ShapeGame2
     /// </summary>
     public partial class SerialConnector : Window
     {
+        SerialPort robotFishPort, feedbackPort;
         public SerialConnector()
         {
             InitializeComponent();
+            RobotPortList.Items.Add(Properties.Settings.Default.RobotFishPort);
+            RobotPortList.SelectedIndex = 0;
+            FeedbackPortList.Items.Add(Properties.Settings.Default.FeedbackPort);
+            FeedbackPortList.SelectedIndex = 0;
+
         }
 
         private void RobotPortList_DropDownOpened(object sender, EventArgs e)
@@ -30,6 +36,48 @@ namespace ShapeGame2
             RobotPortList.Items.Clear();
             foreach (string portName in SerialPort.GetPortNames())
                 RobotPortList.Items.Add(portName);
+        }
+
+        private void FeedbackPortList_DropDownOpened(object sender, EventArgs e)
+        {
+            FeedbackPortList.Items.Clear();
+            foreach (string portName in SerialPort.GetPortNames())
+                FeedbackPortList.Items.Add(portName);
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.RobotFishPort = (string)RobotPortList.SelectedItem;
+            Properties.Settings.Default.FeedbackPort = (string)FeedbackPortList.SelectedItem;
+        }
+
+        private void RobotConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                robotFishPort = new SerialPort((string)RobotPortList.SelectedItem, 115200, Parity.None, 8, StopBits.One);
+                robotFishPort.Open();
+                RobotConnectButton.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FeedbackConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                feedbackPort = new SerialPort((string)FeedbackPortList.SelectedItem, 115200, Parity.None, 8, StopBits.One);
+                feedbackPort.Open();
+                FeedbackConnectButton.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
