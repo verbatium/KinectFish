3546738db08770667ad2959aba6d55cf21e0aade
Valeri Kuzmin <orhdposit@gmail.com>
2011-10-27 14:14:07 +0300

    Fish Position changed to center

--- ShapeGame2/MainWindow.xaml.cs
+++ ShapeGame2/MainWindow.xaml.cs
@@ -95,6 +95,7 @@ namespace ShapeGame2
             }
             this.WindowState = (WindowState)Properties.Settings.Default.WindowState;
             fourLineFish = this.FindName("UCFish") as FourLineFish;
+            Canvas.SetLeft(fourLineFish, Width / 2);
 
             // find a joystick
             try
@@ -515,7 +516,7 @@ namespace ShapeGame2
             screenRect.Y = 0;
             screenRect.Width = playfield.ActualWidth;
             screenRect.Height = playfield.ActualHeight;
-
+            Canvas.SetLeft(fourLineFish, screenRect.Width / 2 - 150);
             vortices.screenResized(playfield.ActualWidth, playfield.ActualHeight);
 
             BannerText.UpdateBounds(screenRect);
